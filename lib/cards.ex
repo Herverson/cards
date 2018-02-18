19d26c4869007d317ff52da1641112d99833b117
Herverson <herverson.sousa@gmail.com>
2018-02-17 22:46:00 -0300

    Criando e embaralhando as cartas

--- lib/cards.ex
+++ lib/cards.ex
@@ -13,6 +13,22 @@ defmodule Cards do
 
   """
   def criar_deck do
-    ["Ás", "Dois", "Três"]
+    valores = ["Ás", "Dois", "Três", "Quatro", "Cinco
+    "]
+    naipes = ["Corações", "Paus", "Ouros", "Espadas"]
+
+    for naipe <- naipes, valor <- valores, do: "#{valor} de #{naipe}"
+  end
+
+  def embaralhar(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contem?(deck, carta) do
+    Enum.member?(deck, carta)
+  end
+
+  def partes(deck, mao) do
+    Enum.split(deck, mao)
   end
 end
