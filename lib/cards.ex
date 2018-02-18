ff9b6bca4b3421ae64a129c3c8a68f077c61b31b
Herverson <herverson.sousa@gmail.com>
2018-02-17 21:13:49 -0300

    Iniciando o projeto

---
+++ lib/cards.ex
@@ -0,0 +1,18 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello
+      :world
+
+  """
+  def criar_deck do
+    ["Ás", "Dois", "Três"]
+  end
+end
